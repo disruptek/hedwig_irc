704a00366043c6f8b3625d788a2e777b6d7d4523
Andy Davidoff <github@andy.disruptek.com>
2021-03-10 15:21:44 -0500

    point to our hedwig, too

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule HedwigIrc.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:hedwig, "~> 1.0"},
+      {:hedwig, git: "https://github.com/disruptek/hedwig.git", tag: "disbot"},
       {:exirc, "~> 1.1.0"},
       {:ex_doc, "~> 0.18", only: :dev}
     ]
